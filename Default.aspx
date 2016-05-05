e89336a812a77a7d5208181825b7edf3aec6d0b5
bankie606 <frcrescioni@gmail.com>
2016-05-05 11:29:37 -0700

    Added files via upload

---
+++ Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Show Tracker</title>
+    <link rel="stylesheet" href="css/StyleSheet.css"/>
+</head>
+<body class="mainbody">
+    <form id="form1" runat="server">
+    <div class="visualcenter">
+    <h1>List of Venues</h1>
+        <asp:GridView ID="GridViewVenues" runat="server"></asp:GridView>
+    </div>
+    <div class="visualcenter">
+     <h1>List of Artists</h1>
+         <asp:GridView ID="GridViewArtists" runat="server"></asp:GridView>
+    </div>
+     <div class="visualcenter">
+     <h1>List of Shows</h1>
+          <asp:GridView ID="GridViewShows" runat="server"></asp:GridView>
+    </div>
+
+        
+    <div class="visualcenter">
+        <h1>Select an Artist to See Where They Are Playing</h1>
+        <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+        <asp:Button ID="Button1" runat="server" Text="Click Here" OnClick="Button1_Click" />
+    </div>
+    <div class="visualcenter">
+        <h1>Select a Venue to See What's Going On</h1>
+        <asp:DropDownList ID="DropDownList2" runat="server"></asp:DropDownList>
+        <asp:GridView ID="GridView2" runat="server"></asp:GridView>
+        <asp:Button ID="Button2" runat="server" Text="Click Here" OnClick="Button2_Click" />
+    </div>
+
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    ShowTrackerServiceReference1.ShowTrackerServiceClient bsc = new ShowTrackerServiceReference1.ShowTrackerServiceClient();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            FillArtistDropDown();
+            FillVenueDropDown();
+            FillArtistsGrid();
+            FillShowGrid();
+            FillVenueGrid();
+        }
+    }
+
+    protected void FillArtistDropDown()
+    {
+        string[] artists = bsc.GetArtists();
+        DropDownList1.DataSource = artists;
+        DropDownList1.DataBind();
+    }
+
+    protected void FillVenueDropDown()
+    {
+        string[] venues = bsc.GetVenues();
+        DropDownList2.DataSource = venues;
+        DropDownList2.DataBind();
+    }
+
+    protected void FillArtistsGrid()
+    {
+        string[] artists = bsc.GetArtists();
+        GridViewArtists.DataSource = artists;
+        GridViewArtists.DataBind();
+    }
+    protected void FillVenueGrid()
+    {
+        string[] venues = bsc.GetVenues();
+        GridViewVenues.DataSource = venues;
+        GridViewVenues.DataBind();
+    }
+    protected void FillShowGrid()
+    {
+        string[] shows = bsc.GetShows();
+        GridViewShows.DataSource = shows;
+        GridViewShows.DataBind();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string artist = DropDownList1.SelectedItem.Text;
+        ShowTrackerServiceReference1.ShowInfoArtist[] artists = bsc.GetShowInfoArtist(artist);
+        GridView1.DataSource = artists;
+        GridView1.DataBind();
+    }
+
+
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        string venue = DropDownList2.SelectedItem.Text;
+        ShowTrackerServiceReference1.ShowInfoVenue[] venues = bsc.GetShowInfoVenue(venue);
+        GridView2.DataSource = venues;
+        GridView2.DataBind();
+    }
+}
